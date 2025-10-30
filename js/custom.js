a3fe3accdbff8247cd1f696e27da762a1685b45a
Brenda <cyriellecoul@gmail.com>
2025-10-30 11:15:01 -0400

    [PF10] Add function to calculate age

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,65 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
-    });
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").fadeOut(1000); // set duration in brackets
+  });
 
-    // CUSTOM LINK
-    $('.custom-link').click(function(){
-    var el = $(this).attr('href');
+  const age = calculateAgeInDays("1988-02-20");
+
+  document.getElementById("myAge").innerHTML = `${age}j`;
+
+  // CUSTOM LINK
+  $(".custom-link").click(function () {
+    var el = $(this).attr("href");
     var elWrapped = $(el);
-    var header_height = $('.navbar').height() + 10;
+    var header_height = $(".navbar").height() + 10;
 
-    scrollToDiv(elWrapped,header_height);
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
-      
-  }
-});
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
 
-  const dialog = document.getElementById('demo-modal');
-  const openBtn = document.getElementById('open-modal');
-  const closeBtn = document.getElementById('close-modal');
+  const dialog = document.getElementById("demo-modal");
+  const openBtn = document.getElementById("open-modal");
+  const closeBtn = document.getElementById("close-modal");
 
-  openBtn.addEventListener('click', function(event) {
+  openBtn.addEventListener("click", function (event) {
     event.preventDefault();
     dialog.showModal();
   });
 
-  closeBtn.addEventListener('click', function() {
+  closeBtn.addEventListener("click", function () {
     dialog.close();
   });
-    
-  })(window.jQuery);
 
+  function calculateAgeInDays(birthDate) {
+    // get today's date
+    const today = new Date();
 
+    // get bithDate format Date
+    const bithDateFormatted = new Date(birthDate);
+
+    // calculate the difference in milliseconds.
+    const diffMillisecondes = today.getTime() - bithDateFormatted.getTime();
+
+    // calculate number of days
+    const days = diffMillisecondes / (1000 * 60 * 60 * 24);
+
+    // returns the age in days (rounded using Math.floor)
+    return Math.floor(days);
+  }
+})(window.jQuery);
